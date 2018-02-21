3aa10518d7ca8ddda953cdc4d7deb45d10ec3f86
tkroon <kroon@kroon.ws>
2018-02-21 00:57:18 +0000

    disconnect running npm from console using nohup

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@ cd /home/pi/slot_node
 sudo modprobe snd_bcm2835
 sudo aplay ./public/sounds/beep-07.wav
 sudo pkill uv4l
+sudo pkill mplayer
 uv4l --driver raspicam --auto-video_nr --encoding yuv420 --width 320 --height 240 --nopreview
 python ./util/barcode.py &
-sudo npm start &
+sudo nohup npm start &
 sleep 15
